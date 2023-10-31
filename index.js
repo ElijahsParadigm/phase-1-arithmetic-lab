ce9d302dcb3dd975a7393ee4ee1fae25e2db30f2
Elijah <ecapers721@gmail.com>
2023-10-31 17:30:01 -0400

    Lab completed Oct 31 2023

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random() * 10)+1;
+
+let num3 = 14
+let num4 = 5
+var mod = num3 % num4
+
+var max = Math.max(2,3,6,8,10,12,14,20,18,16)
